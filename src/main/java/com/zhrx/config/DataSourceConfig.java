3f43ea15e676bb21b6142d463c44e442c40ffe64
xiecw <389190646@qq.com>
2020-03-31 16:02:02 +0800

    [新增][mybatis][基础]修改数据源的配置

---
+++ src/main/java/com/zhrx/config/DataSourceConfig.java
@@ -0,0 +1,21 @@
+package com.zhrx.config;
+
+import org.apache.ibatis.datasource.pooled.PooledDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+    @Bean
+    public DataSource getDataSource(){
+        PooledDataSource dataSource = new PooledDataSource();
+        dataSource.setDriver("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/bd_n?serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("000000");
+        return dataSource;
+
+    }
+}
